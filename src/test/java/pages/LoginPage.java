367d9432eb04486e292f40ad6191628575676a5c
AlyonaChekurova <chekurova97@gmail.com>
2020-10-23 17:39:11 +0300

    Commited on 23.10.2020 Fixed some locators, added comments

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -7,40 +7,82 @@ import org.openqa.selenium.support.PageFactory;
 
 import static helpers.Waiters.clickabilityWait;
 
-
+/**
+ * Класс для работы со страницей авторизации
+ * Идентифицирует поля для ввода логина и пароля, кнопки для продолжения/завершения авторизации
+ * Содержит методы для ввода логина и пароля, нажатия соответствующих кнопок продолжения/завершения авторизации
+ */
 public class LoginPage {
     public WebDriver driver;
 
+    /**
+     * Конструктор класса
+     * Инициализирует активный экземпляр веб-драйвера и элементы на веб-странице
+     *
+     * @param driver - активный экземпляр веб-драйвера
+     */
     public LoginPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
+    /**
+     * Идентификация элементов веб-страницы по соответствующим css-селекторам
+     */
+
+    /**
+     * Поле для ввода логина
+     */
     @FindBy(css = "#identifierId")
     private WebElement loginField;
 
-    @FindBy (css = "#identifierNext button")
+    /**
+     * Кнопка продолжения авторизации
+     */
+    @FindBy(css = "#identifierNext button")
     private WebElement nextBtn;
 
+    /**
+     * Поле для ввода пароля
+     */
     @FindBy(css = "#password input")
     private WebElement passwdField;
 
-    @FindBy (css = "#passwordNext button")
+    /**
+     * Кнопка завершения(подтверждения) авторизации
+     */
+    @FindBy(css = "#passwordNext button")
     private WebElement loginBtn;
 
+    /**
+     * Метод ввода логина в соответствующее поле ввода
+     *
+     * @param login - логин пользователя для ввода
+     */
     public void inputLogin(String login) {
         loginField.sendKeys(login);
     }
 
+    /**
+     * Метод нажатия на кнопку продолжения авторизации
+     */
     public void nextBtnClick() {
         nextBtn.click();
     }
 
+    /**
+     * Метод ввода пароля в соответствующее поле ввода
+     *
+     * @param passwd - пароль пользователя для ввода
+     */
     public void inputPasswd(String passwd) {
         clickabilityWait(this.driver, 5, passwdField);
         passwdField.sendKeys(passwd);
     }
 
+    /**
+     * Метод нажатия на кнопку завершения(подтверждения) авторизации
+     */
     public void loginBtnClick() {
         loginBtn.click();
     }
--- src/test/java/test/GmailTest.java
+++ src/test/java/test/GmailTest.java
@@ -11,16 +11,35 @@ import pages.ProfilePage;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Тестовый класс для проверки работы UI ресурса gmail.com
+ */
 public class GmailTest {
-
+    /**
+     * Объект для работы со страницей авторизации ресурса
+     */
     public static LoginPage loginPage;
+
+    /**
+     * Объект для работы со страницей профиля пользователя ресурса
+     */
     public static ProfilePage profilePage;
+
+    /**
+     * Объект для работы с веб-драйвером
+     */
     public static WebDriver driver;
 
+    /**
+     * Метод инициализации экземпляра веб-драйвера
+     * Создает новый экземпляр веб-драйвера, осуществляет запуск соответствующего драйвера, инициализирует объекты для работы
+     * со страницами ресурса, осуществляет открытие стартовой страницы ресурса, инициализирует неявные ожидания для созданного
+     * экземпляра драйвера
+     */
     @BeforeTest
-    public static void setup(){
+    public static void setup() {
         driver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver",ConfProperties.getProperty("chromedriverPath"));
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriverPath"));
 
         loginPage = new LoginPage(driver);
         profilePage = new ProfilePage(driver);
@@ -31,6 +50,14 @@ public class GmailTest {
         driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
     }
 
+    /**
+     * Метод, содержащий реализацию теста
+     * Осуществляет авторизацию пользователя: ввод логина и пароля, нажатие кнопок для продолжения/завершения авторизации
+     * После успешного завершения авторизации открывает расширенные параметра поиска в почте, заполняет поле "Тема" и запускает
+     * поиск, обрабатывает результаты поиска
+     * Затем выпоняет нажатие на кнопку создания нового письма, заполняет поля "Кому" и "Тема" на панели создания нового электронного
+     * письма, заполняет поле текста письма с использованием результатов поиска,сосуществляет отправку письма
+     */
     @Test
     public void gmailTest() {
         loginPage.inputLogin(ConfProperties.getProperty("login"));
@@ -42,20 +69,23 @@ public class GmailTest {
         profilePage.searchParamBtnClick();
         profilePage.inputSearch(ConfProperties.getProperty("theme"));
         profilePage.searchBtnClick();
+        String letter = profilePage.getSearchRes();
 
         profilePage.newLetterBtnClick();
-        profilePage.inputToField(ConfProperties.getProperty("login"));
+        profilePage.toFieldInput(ConfProperties.getProperty("login"));
         profilePage.topicFieldClick();
-        profilePage.inputTopicField(ConfProperties.getProperty("theme"));
+        profilePage.topicFieldInput(ConfProperties.getProperty("theme"));
         profilePage.letterFieldClick();
-        String letter = profilePage.getSearchRes();
-        profilePage.inputLetter("Найдено писем с темой " + ConfProperties.getProperty("theme") + ": " + letter);
+
+        profilePage.letterInput("Найдено писем с темой " + ConfProperties.getProperty("theme") + ": " + letter);
         profilePage.sendBtnClick();
     }
 
+    /**
+     * Метод, завершающий работу активного экземпляра веб-драйвера
+     */
     @AfterTest
     public static void tearDown() {
         driver.quit();
     }
-
 }
